f7f594cc53f75a26d9fbe4bbf9dd35293e2d43a6
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-01 12:38:00 -0800

    add boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    // run like so: cargo run -- test poem.txt
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {query}");
+
+    println!("In file {filename}");
+
+    let contents = fs::read_to_string(filename)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
