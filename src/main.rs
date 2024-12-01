5ac73f121bc9519f69e3ba418722e9e8319ffcc5
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-01 12:47:16 -0800

    got the first part right

--- src/main.rs
+++ src/main.rs
@@ -14,4 +14,23 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+    let lines = contents.lines();
+
+    let mut list1 = Vec::<i32>::new();
+    let mut list2 = Vec::<i32>::new();
+
+    for line in lines {
+        let data_pair = line.split_whitespace().collect::<Vec<&str>>();
+        let first = data_pair[0];
+        list1.push(first.parse::<i32>().unwrap());
+        let second = data_pair[1];
+        list2.push(second.parse::<i32>().unwrap());
+    }
+    list1.sort();
+    list2.sort();
+    let mut total_distance = 0;
+    for i in 0..list1.len() {
+        total_distance += (list1[i] - list2[i]).abs();
+    }
+    println!("Total distance: {total_distance}");
 }
