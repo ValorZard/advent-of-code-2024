f66b6f9e6126e439b2ee9731c9e988cb034e7c28
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-03 14:36:01 -0800

    we're getting somewhere

--- src/day3.rs
+++ src/day3.rs
@@ -51,13 +51,44 @@ pub fn run_day_3_part_1(contents: String) {
     println!("Total value: {}", total_value);
 }
 
+const DO_STARTER: &str = "do()";
+const DONT_STARTER: &str = "don't()";
+
 pub fn run_day_3_part_2(contents: String) {
     println!("Running day 3 part 1");
     let mut total_value = 0;
     for line in contents.lines() {
         println!("{}", line);
         let indices: Vec<(usize, &str)> = line.match_indices(MUL_FUNC_STARTER).collect();
-        for (index, _) in indices {
+        let do_indices: Vec<(usize, &str)> = line.match_indices(DO_STARTER).collect();
+        let dont_indices: Vec<(usize, &str)> = line.match_indices(DONT_STARTER).collect();
+        // each pair is a (DONT, DO) pair
+        let mut do_pairs = Vec::<(usize, usize)>::new();
+        let mut dont_iterator = dont_indices.into_iter();
+        let mut do_iterator = do_indices.into_iter();
+        let mut current_dont_index = dont_iterator.next().unwrap().0;
+        let mut current_do_index = do_iterator.next().unwrap().0;
+        while do_iterator.len() > 0 && dont_iterator.len() > 0 {
+            if (current_dont_index > current_do_index) {
+                do_pairs.push((current_do_index, current_dont_index));
+                current_do_index = do_iterator.next().unwrap().0;
+                current_dont_index = dont_iterator.next().unwrap().0;
+            }
+            else {
+                current_dont_index = dont_iterator.next().unwrap().0;
+            }
+        }
+
+        let sorted_indices = indices.into_iter().filter(|(index, _)| {
+            for (do_start, dont) in &do_pairs {
+                if *index > *do_start && *index < *dont {
+                    return false;
+                }
+            }
+            true
+        }).collect::<Vec<_>>();
+
+        for (index, _) in sorted_indices {
             let remainder = &line[(index + MUL_FUNC_STARTER.len())..].to_string();
             println!("Index: {}, String: {}", index, remainder);
             if let Ok(value) = seek_parameters_and_multiply(remainder) {
