f6e8e857df6a9c955194a15f5ebbebda787c5ed5
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-03 15:06:00 -0800

    this works but i might have overcomplicated things

--- src/day3.rs
+++ src/day3.rs
@@ -58,47 +58,32 @@ pub fn run_day_3_part_2(contents: String) {
     println!("Running day 3 part 1");
     let mut total_value = 0;
     for line in contents.lines() {
-        println!("{}", line);
-        let indices: Vec<(usize, &str)> = line.match_indices(MUL_FUNC_STARTER).collect();
-        let do_indices: Vec<(usize, &str)> = line.match_indices(DO_STARTER).collect();
+        println!("Starting line: {}", line);
+
         let dont_indices: Vec<(usize, &str)> = line.match_indices(DONT_STARTER).collect();
-        // each pair is a (DONT, DO) pair
-        let mut do_pairs = Vec::<(usize, usize)>::new();
-        let mut dont_iterator = dont_indices.iter();
-        let mut do_iterator = do_indices.iter();
+        let do_indices: Vec<(usize, &str)> = line.match_indices(DO_STARTER).collect();
 
-        if let Some(dont_index) = dont_iterator.next() {
-            if let Some(do_index) = do_iterator.next() {
-                if dont_index.0 < do_index.0 {
-                    do_pairs.push((dont_index.0, do_index.0));
-                    
-                    while let Some(dont_index) = dont_iterator.next() {
-                        if let Some(do_index) = do_iterator.next() {
-                            if dont_index.0 < do_index.0 {
-                                do_pairs.push((dont_index.0, do_index.0));
-                            }
-                        }
-                    }
+        let mut new_line = line.to_string();
+
+        let mut dont_iter = dont_indices.iter();
+        let mut do_iter = do_indices.iter();
+        'outer: while let Some((dont_index, _)) = dont_iter.next() {
+            while let Some((do_index,_)) = do_iter.next() {
+                if dont_index < do_index && *do_index < new_line.len(){
+                    let first_half = new_line[..*dont_index].to_string();
+                    let second_half = &new_line[*do_index..];
+                    new_line = first_half + second_half;
+                    continue 'outer;
                 }
             }
+            new_line = new_line[..*dont_index].to_string();
         }
+        println!("New line: {}", new_line);
 
-        for (dont_index, do_index) in &do_pairs {
-            println!("Dont index: {}, Do: {}", dont_index, do_index);
-        }
+        let indices: Vec<(usize, &str)> = new_line.match_indices(MUL_FUNC_STARTER).collect();
 
-
-        let sorted_indices = indices.into_iter().filter(|(index, _)| {
-            for (dont_index, do_index) in &do_pairs {
-                if *index > *dont_index && *index < *do_index {
-                    return false;
-                }
-            }
-            true
-        }).collect::<Vec<_>>();
-
-        for (index, _) in sorted_indices {
-            let remainder = &line[(index + MUL_FUNC_STARTER.len())..].to_string();
+        for (index, _) in indices {
+            let remainder = &new_line[(index + MUL_FUNC_STARTER.len())..].to_string();
             println!("Index: {}, String: {}", index, remainder);
             if let Ok(value) = seek_parameters_and_multiply(remainder) {
                 println!("Value: {}", value);
