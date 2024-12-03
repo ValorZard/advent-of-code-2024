b4310955b5aac55d8caf7adc1880474d23053389
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-03 13:55:52 -0800

    actually begin part 1

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,12 @@
+const mul_func: &str = "mul(";
+
 pub fn run_day_3_part_1(contents: String) {
     println!("Running day 3 part 1");
+    for line in contents.lines() {
+        println!("{}", line);
+        let indices: Vec<(usize, &str)> = line.match_indices(mul_func).collect();
+        for (index, _) in indices {
+            println!("Index: {}", index);
+        }
+    }
 }
