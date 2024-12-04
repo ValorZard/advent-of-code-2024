44d9a0823a6ea19bc0bc8ae739f241edce4bd79d
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-04 12:43:02 -0800

    now we're getting somewhere

--- src/day4.rs
+++ src/day4.rs
@@ -2,27 +2,24 @@ use std::fmt::Display;
 
 const XMAS_STR: [char; 4] = ['X', 'M', 'A', 'S'];
 
-fn run_xmas_checker(line: &str) -> bool {
-    for (i, c) in line.chars().enumerate() {
-        if XMAS_STR.contains(&c) {
-            return true;
-        }
-    }
-    false
+fn run_xmas_checker(lines: &Vec<&str>, is_reversed: bool, x: usize, y: usize) -> i32 {
+    println!("Checking xmas at {}, {} -> {}", x, y, lines[y].chars().nth(x).unwrap());
+    0
 }
 
 pub fn run_day_4_part_1(contents: &str) -> impl Display + use<'_> {
-    let lines = contents.lines();
+    let lines : Vec<&str> = contents.lines().collect();
     let mut total_xmas = 0;
     //let mut possible_xmas : Vec<Vec<char>> = Vec::new();
-    'outer: for line in contents.lines() {
+    for y in 0..lines.len() {
+        let line = lines[y];
         println!("{}", line);
-        for (i, c) in line.chars().enumerate() {
-            if c == XMAS_STR[0] || c == XMAS_STR[3] {
-                if run_xmas_checker(line) {
-                    total_xmas += 1;
-                    continue 'outer;
-                }
+        for (x, c) in line.chars().enumerate() {
+            if c == XMAS_STR[0] {
+                run_xmas_checker(&lines, false, x, y);
+            }
+            else if c == XMAS_STR[3] {
+                run_xmas_checker(&lines, true, x, y);
             }
         }
     }
