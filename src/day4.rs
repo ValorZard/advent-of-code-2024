5ad0c701d93b8760ec4f8a1d91c54a327149c9eb
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-04 12:08:42 -0800

    this is totally wrong, but commiting for posterity

--- src/day4.rs
+++ src/day4.rs
@@ -1,7 +1,21 @@
 use std::fmt::Display;
 
+const XMAS_STR: [char; 4] = ['X', 'M', 'A', 'S'];
+
 pub fn run_day_4_part_1(contents: &str) -> impl Display + use<'_> {
-    contents
+    let lines = contents.lines();
+    let mut total_xmas = 0;
+    //let mut possible_xmas : Vec<Vec<char>> = Vec::new();
+    'outer: for line in contents.lines() {
+        println!("{}", line);
+        for (i, c) in line.chars().enumerate() {
+            if XMAS_STR.contains(&c) {
+                total_xmas += 1;
+                continue 'outer;
+            }
+        }
+    }
+    total_xmas
 }
 
 pub fn run_day_4_part_2(contents: &str) -> impl Display + use<'_> {
