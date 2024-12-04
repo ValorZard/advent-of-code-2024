452630bf830c43680db64d92e1e11cb02da28005
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-04 12:36:17 -0800

    random junk logic but we're getting somewhere

--- src/day4.rs
+++ src/day4.rs
@@ -2,6 +2,15 @@ use std::fmt::Display;
 
 const XMAS_STR: [char; 4] = ['X', 'M', 'A', 'S'];
 
+fn run_xmas_checker(line: &str) -> bool {
+    for (i, c) in line.chars().enumerate() {
+        if XMAS_STR.contains(&c) {
+            return true;
+        }
+    }
+    false
+}
+
 pub fn run_day_4_part_1(contents: &str) -> impl Display + use<'_> {
     let lines = contents.lines();
     let mut total_xmas = 0;
@@ -9,9 +18,11 @@ pub fn run_day_4_part_1(contents: &str) -> impl Display + use<'_> {
     'outer: for line in contents.lines() {
         println!("{}", line);
         for (i, c) in line.chars().enumerate() {
-            if XMAS_STR.contains(&c) {
-                total_xmas += 1;
-                continue 'outer;
+            if c == XMAS_STR[0] || c == XMAS_STR[3] {
+                if run_xmas_checker(line) {
+                    total_xmas += 1;
+                    continue 'outer;
+                }
             }
         }
     }
