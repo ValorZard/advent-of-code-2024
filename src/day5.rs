41841873387847db4e92eabe66c2414679d26200
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-05 12:57:01 -0800

    i hate parsing data

--- src/day5.rs
+++ src/day5.rs
@@ -1,7 +1,8 @@
 use std::fmt::Display;
 
 pub fn run_day_5_part_1(contents: &str) -> impl Display + use<'_> {
-    contents
+    let parts = contents.split("\n").collect::<Vec<&str>>();
+    parts[0]
 }
 
 pub fn run_day_5_part_2(contents: &str) -> impl Display + use<'_> {
