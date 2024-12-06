94b1cc589173549f34dd2ccbc116b667aed77d71
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-06 12:22:48 -0800

    yaay brute forcing

--- src/day6.rs
+++ src/day6.rs
@@ -3,19 +3,28 @@ use std::fmt::Display;
 pub fn run_day_6_part_1(contents: &str) -> impl Display + use<'_> {
     // convert to vec of byte array
     let mut guard_map = contents.lines().map(|x| x.as_bytes().to_vec()).collect::<Vec<Vec<u8>>>();
-    for y in 0..guard_map.len() {
-       for x in 0..guard_map[y].len() {
-            if char::from(guard_map[y][x]) == '^' {
-                if let Some(new_y) = y.checked_add_signed(-1) {
-                    if guard_map[new_y][x] != b'#' as u8 {
-                        guard_map[new_y][x] = '^' as u8;
-                        guard_map[y][x] = 'X' as u8;
+    let mut finished = false;
+    while !finished {
+        for y in 0..guard_map.len() {
+            for x in 0..guard_map[y].len() {
+                    if char::from(guard_map[y][x]) == '^' {
+                        if let Some(new_y) = y.checked_add_signed(-1) {
+                            if guard_map[new_y][x] != b'#' as u8 {
+                                guard_map[new_y][x] = '^' as u8;
+                                guard_map[y][x] = 'X' as u8;
+                            }
+                            else {
+                                finished = true;
+                            }
+                        }
+                        else {
+                            finished = true;
+                        }
                     }
-                }
+                print!("{}", guard_map[y][x] as char);
             }
-           print!("{}", guard_map[y][x] as char);
-       }
-       println!()
+            println!()
+        }
     }
     println!("full map");
     for y in 0..guard_map.len() {
