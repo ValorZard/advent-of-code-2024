7d0ea6c2c21c4961b1bbc892161de5db5168d60c
Srayan Jana <valorzard.kiko@outlook.com>
2024-12-06 13:16:47 -0800

    almost there

--- src/day6.rs
+++ src/day6.rs
@@ -29,6 +29,20 @@ pub fn run_day_6_part_1(contents: &str) -> impl Display + use<'_> {
                             }
                             else {
                                 guard_map[y][x] = 'v' as u8;
+                            }
+                        }
+                        else {
+                            finished = true;
+                        }
+                    }
+                    else if char::from(guard_map[y][x]) == 'v' {
+                        if y + 1 < guard_map[y].len() {
+                            if guard_map[y + 1][x] != b'#' as u8 {
+                                guard_map[y + 1][x] = 'v' as u8;
+                                guard_map[y][x] = 'X' as u8;
+                            }
+                            else {
+                                guard_map[y][x] = '<' as u8;
                                 finished = true;
                             }
                         }
